6a7e751d659caa20f76de201f22e5c13033b5f75
Sebastian Thorup Frederiksen <sthorupf@hotmail.com>
2018-09-26 10:02:55 +0200

    Cleaned up the menuspec.txt file with code.

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SmartMenuLibrary
 {
     public class SmartMenu
     {
-        public void LoadMenu(string path) //vi kan skrive i consolen herfra
+        public void LoadMenu(string path) //We can write to the console from here
         {
-            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\datamatiker\SmartMenu\SmartMenuLibrary\" + path); 
-            Console.WriteLine("Hello world");
+            string file = File.ReadAllText(@"D:\datamatiker\SmartMenu\SmartMenuLibrary\" + path);           
+                        
+            string[] MenuSpecsArray = file.Split(';');
+            string clean = "";
+            for (int i = 0; i < MenuSpecsArray.Length; i++)
+            {
+                clean = Regex.Replace(MenuSpecsArray[i], "[^A-Z a-z 0-9 ]", "");
+                Console.WriteLine(clean);
+            }
+            Console.ReadKey();
+            
         }
-        public void Activate()
+        public void Activate() //We can write to the console from here
         {
-            // Implement ...
+            
         }
     }
 }
