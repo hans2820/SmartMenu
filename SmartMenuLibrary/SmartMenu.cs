a403758d6b354a5cb6fd63024c6e02a160751689
Sebastian Thorup Frederiksen <sthorupf@hotmail.com>
2018-09-26 11:26:24 +0200

    Removed an unused line of code.

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -15,10 +15,9 @@ namespace SmartMenuLibrary
             string file = File.ReadAllText(@"D:\datamatiker\SmartMenu\SmartMenuLibrary\" + path);           
                         
             string[] MenuSpecsArray = file.Split(';');
-            string clean = "";
             for (int i = 0; i < MenuSpecsArray.Length; i++)
             {
-                clean = Regex.Replace(MenuSpecsArray[i], "[^A-Z a-z 0-9 \r\n]", "");
+                string clean = Regex.Replace(MenuSpecsArray[i], "[^A-Å a-å 0-9 \r\n]", "");
                 Console.Write(clean+" ");
             }
             Console.WriteLine("");                      
@@ -32,7 +31,7 @@ namespace SmartMenuLibrary
             {
                 binding.exit();
             }
-            else if(0 != number)
+            else if(number != 0)
             {
                 res = binding.call(number);
             }
